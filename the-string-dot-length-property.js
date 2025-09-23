67a37296a946abdc20c5529a612d0eb810a862c9
agartorie-bit <agar.torie@gmail.com>
2025-09-23 14:15:06 -0500

    Finished the assignment

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -15,16 +15,26 @@ Your task is to:
 
 // Starter Code (Pre-filled):
 // Predefined strings
+//let stringOne = "Coding Bootcamp";
+//let stringTwo = "JavaScript";
+//let stringThree = "";
+
+// Your task: Assign the length of each string to the corresponding variable
+//let lengthOne;   // Length of stringOne
+//let lengthTwo;   // Length of stringTwo
+//let lengthThree; // Length of stringThree
+
+// Your code here
+
+
 let stringOne = "Coding Bootcamp";
 let stringTwo = "JavaScript";
 let stringThree = "";
 
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
-
-// Your code here
-
-
-
+let lengthOne = stringOne.length;   // Length of stringOne
+let lengthTwo = stringTwo.length;   // Length of stringTwo
+let lengthThree = stringThree.length; // Length of stringThree
+console.log (lengthOne);
+console.log (lengthTwo);
+console.log (lengthThree);
